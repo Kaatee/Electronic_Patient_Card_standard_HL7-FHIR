ed599a9c482e5abbe5d346db9480219617b54463
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-05-19 11:14:41 +0200

    Add: connection class, change set patient to arraylist

---
+++ src/sample/Connection.java
@@ -0,0 +1,78 @@
+package sample;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.resource.Bundle;
+import ca.uhn.fhir.model.dstu2.resource.Patient;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Connection {
+
+    private String serverBase;
+    private IGenericClient client;
+    private FhirContext ctx;
+    private ArrayList<Patient> patientList;
+    private Bundle responseFromServer;
+
+    public Connection()
+    {
+        this.ctx = FhirContext.forDstu2();
+        this.serverBase="http://fhirtest.uhn.ca/baseDstu2";
+        this.client = ctx.newRestfulGenericClient(serverBase);
+        this.patientList  = new ArrayList<>();
+    }
+
+    private void requestForServer (){
+        //zapytanie
+        responseFromServer = client
+                .search()
+                .forResource(Patient.class)
+                .where(Patient.FAMILY.matches().value("Smith"))
+                .returnBundle(Bundle.class)
+                .execute();
+    }
+
+    private static void addInitialUrlsToSet(Bundle theBundle, ArrayList<Patient> thepatientList) {
+        for (int i=0 ; i<theBundle.getEntry().size(); i++){
+            thepatientList.add((Patient) theBundle.getEntry().get(i).getResource());
+        }
+    }
+
+    private static void addAnyResourcesNotAlreadyPresentToBundle( Bundle thePartialBundle, ArrayList<Patient> thepatientList) {
+        for (int i=0 ; i<thePartialBundle.getEntry().size(); i++){
+            if (!thepatientList.contains((Patient) thePartialBundle.getEntry().get(i).getResource())) {
+                thepatientList.add((Patient) thePartialBundle.getEntry().get(i).getResource());
+            }
+        }
+    }
+
+
+    public void makeResult () {
+
+        requestForServer();
+
+        addInitialUrlsToSet(responseFromServer, patientList);
+        Bundle partialBundle = responseFromServer;
+        for (; ; ) {
+            if (partialBundle.getLink(IBaseBundle.LINK_NEXT) != null) {
+                partialBundle = client.loadPage().next(partialBundle).execute();
+                addAnyResourcesNotAlreadyPresentToBundle(partialBundle, patientList);
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("Pacjentow po wyszukiwaniu: " + patientList.size());
+//        for (Iterator<Patient> it = patientList.iterator(); it.hasNext(); ) {
+//            Patient f = it.next();
+//         ;
+//            System.out.println(f.getName());
+//
+//        }
+        patientList.forEach((a)->{   System.out.println("--- PATIENT  ---");
+            System.out.println(a.getName());});
+
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,91 +1,32 @@
 package sample;
 
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.dstu2.resource.Bundle;
-import ca.uhn.fhir.model.dstu2.resource.Patient;
-import ca.uhn.fhir.model.primitive.IdDt;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
-import org.hl7.fhir.instance.model.api.IBaseBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.awt.*;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
-public class Main{ //extends Application {
+public class Main extends Application {
 
-    /*
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 650));
         primaryStage.show();
-    }*/
-
-    private static void addInitialUrlsToSet(Bundle theBundle, Set<Patient> theResourcesAlreadyAdded) {
-//        for (Bundle.Entry entry : theBundle.getEntry()) {
-//            theResourcesAlreadyAdded.add(entry.getResource());
-//        }
-
-        for (int i=0 ; i<theBundle.getEntry().size(); i++){
-            theResourcesAlreadyAdded.add((Patient) theBundle.getEntry().get(i).getResource());
-        }
     }
 
-    private static void addAnyResourcesNotAlreadyPresentToBundle( Bundle thePartialBundle, Set<Patient> theResourcesAlreadyAdded) { //Bundle theAggregatedBundle,
-//        for (Bundle.Entry entry : thePartialBundle.getEntry()) {
-//            if (!theResourcesAlreadyAdded.contains(entry.getFullUrl())) {
-//                theResourcesAlreadyAdded.add(entry.getFullUrl());
-//                theAggregatedBundle.getEntry().add(entry);
-//            }
-//        }
 
-        for (int i=0 ; i<thePartialBundle.getEntry().size(); i++){
-            //theResourcesAlreadyAdded.add((Patient) theBundle.getEntry().get(i).getResource());
-            if (!theResourcesAlreadyAdded.contains((Patient) thePartialBundle.getEntry().get(i).getResource())) {
-                theResourcesAlreadyAdded.add((Patient) thePartialBundle.getEntry().get(i).getResource());
-              // theAggregatedBundle.getEntry().add((Patient) thePartialBundle.getEntry());
-           }
-           }
-    }
+
     public static void main(String[] args) {
-        // launch(args);
-        // We're connecting to a DSTU1 compliant server in this example
-        FhirContext ctx = FhirContext.forDstu2();
-        String serverBase = "http://fhirtest.uhn.ca/baseDstu2";
+        launch(args);
 
-        IGenericClient client = ctx.newRestfulGenericClient(serverBase);
-
-        Bundle results = client
-                .search()
-                .forResource(Patient.class)
-                .where(Patient.FAMILY.matches().value("Smith"))
-                .returnBundle(Bundle.class)
-                .execute();
-        System.out.println("Found " + results.getEntry().size()+ " patients named 'Smith'");
-
-        Set<Patient> resourcesAlreadyAdded = new HashSet<Patient>();
-        addInitialUrlsToSet(results, resourcesAlreadyAdded);
-        Bundle partialBundle = results;
-        for (;;) {
-            if (partialBundle.getLink(IBaseBundle.LINK_NEXT) != null) {
-                partialBundle = client.loadPage().next(partialBundle).execute();
-                addAnyResourcesNotAlreadyPresentToBundle( partialBundle, resourcesAlreadyAdded);
-            } else {
-                break;
-            }
-        }
-
-        System.out.println(resourcesAlreadyAdded.size());
-
-
-        for (Iterator<Patient> it = resourcesAlreadyAdded.iterator(); it.hasNext(); ) {
-            Patient f = it.next();
-            System.out.println(f.getName());
-            System.out.println(f.getId());
-        }
+        //connect to server
+        Connection myConnect = new Connection();
+        myConnect.makeResult();
 
     }
 }
